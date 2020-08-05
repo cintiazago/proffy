118adb709f41321685697019b1f02c9a3011baed
Cintia Zago <cintiazago@gmail.com>
2020-08-05 18:33:07 -0300

    Implementing components

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+import PageHeader from '../../components/PageHeader';
 import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+const TeacherList: React.FC = () => {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">
+              Matéria
+              <input id="subject" name="subject" type="text" />
+            </label>
+          </div>
 
-function TeatcherList() {
-    return (
-        <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
+          <div className="input-block">
+            <label htmlFor="week_day">
+              Dia da semana
+              <input type="text" id="week_day" />
+            </label>
+          </div>
 
-                <div className="header-content">
-                    <strong>Estes são os proffys disponíveis.</strong>
-                </div>
-            </header>
-        </div>
-    )
-}
+          <div className="input-block">
+            <label htmlFor="time">
+              Hora
+              <input type="text" id="time" />
+            </label>
+          </div>
+        </form>
+      </PageHeader>
 
-export default TeatcherList;
+      <main>
+        <TeacherItem />
+      </main>
+    </div>
+  );
+};
+
+export default TeacherList;
